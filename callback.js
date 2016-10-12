8e7002cc5f7f204f4e68d97015fea6ba4a079dfc
Erick Carvajal R <neocarvajal12@gmail.com>
2016-10-12 20:33:51 +0000

    update

--- callback.js
+++ callback.js
@@ -1,29 +1,31 @@
 var OAUTH_URL = "https://github.com/login/oauth/authorize?scope=user:email&client_id=";
 var CLIENT_ID = "4ad89144980ebf7ce5f7";
+var OAUTH_CODE = " ";
 
 function start(oauth, client_id) {
 
 	function get_data(){
+
 		// Sending a receiving data in JSON format using GET method		
 		xhr = new XMLHttpRequest();
-		var url = oauth+client_id;
-		//var url = "url?data=" + encodeURIComponent(JSON.stringify({"user":"","password":""}));
+		//var url = oauth+client_id;
+		var url = "url?data=" + encodeURIComponent(JSON.stringify({"code":""}));
 		xhr.open("GET", url, true);
 		xhr.setRequestHeader("Content-type", "application/json");
 		xhr.onreadystatechange = function () { 
 		    if (xhr.readyState == 4 && xhr.status == 200) {
 		        var json = JSON.parse(xhr.responseText);
-		        console.log(json.client_id + ", " + json.user + ", " + json.password)
+		        console.log(json.code + ",");
 		    }
 		}
-		xhr.send(client_id);
+		//xhr.send(client_id);
 	}
 
-/*	function send_data(){
+	function send_data(){
 		// Sending and receiving data in JSON format using POST mothod
 
 		xhr = new XMLHttpRequest();
-		var url = "https://github.com/login/oauth/access_token";
+		var url = "https://github.com/login/oauth/access_token?scope=user:email";
 		xhr.open("POST", url, true);
 		xhr.setRequestHeader("Content-type", "application/json");
 		xhr.onreadystatechange = function () { 
@@ -35,6 +37,6 @@ function start(oauth, client_id) {
 		var data = JSON.stringify({"client_id":"","password":"101010"});
 		xhr.send(data);
 	}
-*/
+
 	get_data();
 }
