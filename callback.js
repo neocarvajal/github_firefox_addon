992a0fe40c05d5726e954cae41102adc626af2cb
Erick Carvajal R <neocarvajal12@gmail.com>
2016-10-12 22:33:31 +0000

    update

--- callback.js
+++ callback.js
@@ -2,17 +2,19 @@ var OAUTH_URL = "https://github.com/login/oauth/authorize?scope=";
 var OAUTH_SCOPE = "user:email + "&" + client_id=";
 var CLIENT_ID = "4ad89144980ebf7ce5f7";
 
-function start(oauth_url, oauth_scope, client_id) {
+window.onload = function main () {
+   var url = OAUTH_URL + OAUTH_SCOPE + CLIENT_ID;
+   var login = document.getElementById('access');
+   login.click(function(url){
+      console.log(url)
+      window.open(url);
+   });
 
-   function get_data(){
-	
-      var login = window.getElementById('access');
-      login.click(function (){
-         window.open('oauth_url + oauth_scope + client_id');
-      });
-     
+   function get_code() 
+   {
       window.addEventListener('message', function (event) {
-         var code = event.data;           
+         var code = event.data;
          console.log(code);
-      });
+      });   
+   }
 }
