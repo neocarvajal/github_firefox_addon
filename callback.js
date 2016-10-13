e51b6a35756facb2dea5dfc414e9e22a5662e0a2
Erick Carvajal R <neocarvajal12@gmail.com>
2016-10-13 01:05:28 +0000

    update

--- callback.js
+++ callback.js
@@ -3,10 +3,10 @@ const OAUTH_SCOPE = "user:email + "&" + client_id=";
 const CLIENT_ID = "4ad89144980ebf7ce5f7";
 
 window.onload = function() {
-   init(OAUTH_URL, OAUTH_SCOPE, CLIENT_ID);
+   main(OAUTH_URL, OAUTH_SCOPE, CLIENT_ID);
 }
 
-function init(oauth_url, oauth_scope, client_id) {   
+function main(oauth_url, oauth_scope, client_id) {   
 
    var login = document.getElementById('access');
    var url = oauth_url + oauth_scope + client_id;
