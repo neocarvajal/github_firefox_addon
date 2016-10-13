5ac82f83bf7bd54f1a57658b09558cec1bab3577
Erick Carvajal R <neocarvajal12@gmail.com>
2016-10-13 01:04:19 +0000

    update

--- callback.js
+++ callback.js
@@ -12,7 +12,7 @@ function init(oauth_url, oauth_scope, client_id) {
    var url = oauth_url + oauth_scope + client_id;
    login.onclick = function(url){
       console.log(url);
-      window.location(url);
+      window.location('url');
    }
 //   window.addEventListener('message',function (event) {
 //      var code = event.data;
