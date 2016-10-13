17dcd35a6f6599383a3cff6d9f8d41b35cc7eff4
Erick Carvajal R <neocarvajal12@gmail.com>
2016-10-13 00:08:29 +0000

    update

--- callback.js
+++ callback.js
@@ -1,6 +1,6 @@
-var OAUTH_URL = "https://github.com/login/oauth/authorize?scope=";
-var OAUTH_SCOPE = "user:email + "&" + client_id=";
-var CLIENT_ID = "4ad89144980ebf7ce5f7";
+const OAUTH_URL = "https://github.com/login/oauth/authorize?scope=";
+const OAUTH_SCOPE = "user:email + "&" + client_id=";
+const CLIENT_ID = "4ad89144980ebf7ce5f7";
 
 window.onload = function() {
    var url = OAUTH_URL + OAUTH_SCOPE + CLIENT_ID;
