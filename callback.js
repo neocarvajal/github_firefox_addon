4f03a97222e35944c4e6d68477451c74b2be1cc9
Erick Carvajal R <neocarvajal12@gmail.com>
2016-10-13 00:15:59 +0000

    update

--- callback.js
+++ callback.js
@@ -4,14 +4,15 @@ const CLIENT_ID = "4ad89144980ebf7ce5f7";
 
 window.onload = function() {
    var url = OAUTH_URL + OAUTH_SCOPE + CLIENT_ID;
-   var login = document.getElementById('access');
+   var login = document.getElementById("access");
    login.onclick(function(){
       console.log(url);
-      window.location(url);
+      window.open(url);
    });
-   
-   window.addEventListener('message',function (event) {
-      var code = event.data;
-      console.log(code);
-   });  
+
+   (function(){
+      window.addEventListener('message',function (event) {
+         var code = event.data;
+         console.log(code);	
+   })();
 }
