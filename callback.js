0245f8f7c51dd440a90c32f0a78330ac709633eb
Erick Carvajal R <neocarvajal12@gmail.com>
2016-10-13 00:58:47 +0000

    update

--- callback.js
+++ callback.js
@@ -8,7 +8,7 @@ window.onload = function() {
 
 function init(oauth_url, oauth_scope, client_id) {   
 
-   var url = OAUTH_URL + OAUTH_SCOPE + CLIENT_ID;
+   var url = oauth_url + oauth_scope + client_id;
    var login = document.getElementById('access');
    console.log(login.value);
    login.onclick = function(url){
