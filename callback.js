0510ab021aae8cb38caf0e9e786d454bb993d582
Erick Carvajal R <neocarvajal12@gmail.com>
2016-10-30 23:23:25 +0000

    update

--- callback.js
+++ callback.js
@@ -33,8 +33,8 @@ function main(oauth_url, oauth_scope, client_id) {
 			var urlRequest = OAUTH_SERVE + "?code=";
 			var httpRequest = new XMLHttpRequest();    
 			httpRequest.open('GET', OAUTH_SERVE, true);
-			httpRequest.withCredentials = true;
-    		httpRequest.onreadystatechange = handler;
+			
+    		
 			httpRequest.send(urlRequest + code);
 			
 			httpRequest.onreadystatechange = function () {
